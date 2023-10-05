354184e962cebee9b3b7d6df99597020beaacaf3
adryvitoria <130464991+adryvitoria@users.noreply.github.com>
2023-10-05 09:12:39 -0300

    05/10

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 alert('jady');
+
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSompom(){ document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+function tocaSomTim(){ document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_Puff').onclick = tocaSomPuff;
+function tocaSomPuff(){ document.querySelector('#som_tecla_Puff').play();
+}
+document.querySelector('.tecla_splash').onclick = tocaSomsplash;
+function tocaSomsplash(){ document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('.tecla_toim').onclick = tocaSomtoim;
+function tocaSomtoim(){ document.querySelector('#som_tecla_toim').play();
+}
